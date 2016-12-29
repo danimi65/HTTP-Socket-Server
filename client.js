4072efc762a4025d5b7fd901f7e89560d29365db
Andrea <takamiya.andrea@gmail.com>
2016-12-28 22:36:04 -1000

    Getting body contents out in the response

--- client.js
+++ client.js
@@ -3,10 +3,17 @@ const net = require('net');
 // v--------socket connection to server
 
 var server = net.createConnection(8080, 'localhost',() => {
-  console.log('Connected to:');
-  // process.stdout.write('Enter your username:');
+  console.log('Connected');
 });
 
 server.on('connect', () => {
   process.stdin.pipe(server);
+
+  server.on('data', (request) => {
+    process.stdout.write(request);
+  });
+
+  server.end('data', () => {
+    console.log('Connection closed');
+  });
 });
--- server.js
+++ server.js
@@ -1,22 +1,30 @@
 const net = require('net');
 const fs = require('fs');
-const PORT = 8080;
 const timestamp = new Date();
 
 //Start a server
-var server = net.createServer((socket) => {
+var server = net.createServer((socket, res) => {
+
 
   socket.setEncoding('utf8');
   socket.on('data', (chunk) => {
-    var response = `Status Code: \n
+    var response = `Status Code:\n
     Date: ${timestamp}\n
-    Server: Andreas Server\n`;
+    Server: My Custom Server\n`;
 
     socket.write(response);
 
-    console.log('chunk', chunk);
-    socket.end();
+    var url = './index.html';
+
+    fs.readFile(url, 'utf8', (err, data) => {
+      if (err) throw err;
+      console.log(data);
+    });
+    socket.end('data', () => {
+    console.log('Connection closed');
+    });
   });
+
 });
 
 server.listen(8080, 'localhost', () => {
