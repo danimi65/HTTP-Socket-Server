0af41c60a815c2267a70e589004742021d9bc168
Andrea <takamiya.andrea@gmail.com>
2016-12-28 23:32:23 -1000

    Moving to Module exports way

--- server.js
+++ server.js
@@ -1,38 +1,38 @@
 const net = require('net');
 const fs = require('fs');
 const timestamp = new Date();
-const url = require('url')
+const url = require('url');
+const staticContent = require('./staticContent');
 
 //Start a server
-var server = net.createServer((socket, res) => {
-
+var server = net.createServer((socket) => {
 
   socket.setEncoding('utf8');
   socket.on('data', (chunk) => {
     var response = `Status Code:\n
     Date: ${timestamp}\n
     Server: My Custom Server\n`;
-
+    console.log(chunk);
     socket.write(response);
 
-    if ('./index.html') {
+
+    staticContent.index_html;
+    //HTTP Response back
+
       fs.readFile('./index.html', 'utf8', (err, data) => {
         if (err) throw err;
         console.log(data);
       });
-    }
-    else if (filePath === './hydrogen.html') {
+
       fs.readFile('./hydrogen.html', 'utf8', (err, data) => {
         if (err) throw err;
         console.log(data);
       });
-    }
-    else if (filePath === './helium.html') {
-      fs.readFile('./hydrogen.html', 'utf8', (err, data) => {
+
+      fs.readFile('./helium.html', 'utf8', (err, data) => {
         if (err) throw err;
         console.log(data);
       });
-    }
 
     socket.end('data', () => {
     console.log('Connection closed');
