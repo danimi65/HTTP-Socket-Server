7a25623c4bb3fc1a423d95ec3ac5c59594aefa55
Andrea <takamiya.andrea@gmail.com>
2016-12-28 21:09:23 -1000

    Added the hardcoded message as the response for localhost:8080

--- server.js
+++ server.js
@@ -1,15 +1,22 @@
 const net = require('net');
+const fs = require('fs');
 const PORT = 8080;
+const timestamp = new Date();
 
 //Start a server
 var server = net.createServer((socket) => {
 
-  socket.on('data', () => {
+  socket.setEncoding('utf8');
+  socket.on('data', (chunk) => {
+    var response = `Status Code: \n
+    Date: ${timestamp}\n
+    Server: Andreas Server\n`;
 
+    socket.write(response);
+
+    console.log('chunk', chunk);
+    socket.end();
   });
-
-  socket.end();
-
 });
 
 server.listen(8080, 'localhost', () => {
