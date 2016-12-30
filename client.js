81b208c503ac32cb0d5519b15ff8526407df6dd6
Andrea <takamiya.andrea@gmail.com>
2016-12-29 17:51:04 -1000

    Reverted back to when just the header request printed out

--- client.js
+++ client.js
@@ -9,9 +9,9 @@ var server = net.createConnection(8080, 'localhost',() => {
 server.on('connect', () => {
   process.stdin.pipe(server);
 
-  server.on('data', (request) => {
-    process.stdout.write(request);
-  });
+  // server.on('data', (request) => {
+  //   process.stdout.write(request);
+  // });
 
   server.end('data', () => {
     console.log('Connection closed');
--- server.js
+++ server.js
@@ -2,45 +2,46 @@ const net = require('net');
 const fs = require('fs');
 const timestamp = new Date();
 const url = require('url');
-const staticContent = require('./staticContent');
+// const staticContent = require('./staticContent');
 
 //Start a server
 var server = net.createServer((socket) => {
 
   socket.setEncoding('utf8');
+
   socket.on('data', (chunk) => {
     var response = `Status Code:\n
     Date: ${timestamp}\n
     Server: My Custom Server\n`;
-    console.log(chunk);
+    // console.log(chunk);
     socket.write(response);
 
 
-    staticContent.index_html;
-    staticContent.helium_html;
-    staticContent.hydrogen_html;
-    staticContent.error_html;
+  //   staticContent.index_html;
+  //   staticContent.helium_html;
+  //   staticContent.hydrogen_html;
+  //   staticContent.error_html;
 
-    //HTTP Response back
+  //   //HTTP Response back
 
-      fs.readFile('./index.html', 'utf8', (err, data) => {
-        if (err) throw err;
-        console.log(data);
-      });
+  //     fs.readFile('./index.html', 'utf8', (err, data) => {
+  //       if (err) throw err;
+  //       console.log(data);
+  //     });
 
-      fs.readFile('./hydrogen.html', 'utf8', (err, data) => {
-        if (err) throw err;
-        console.log(data);
-      });
+  //     fs.readFile('./hydrogen.html', 'utf8', (err, data) => {
+  //       if (err) throw err;
+  //       console.log(data);
+  //     });
 
-      fs.readFile('./helium.html', 'utf8', (err, data) => {
-        if (err) throw err;
-        console.log(data);
-      });
+  //     fs.readFile('./helium.html', 'utf8', (err, data) => {
+  //       if (err) throw err;
+  //       console.log(data);
+  //     });
 
-    socket.end('data', () => {
-    console.log('Connection closed');
-    });
+  //   socket.end('data', () => {
+  //   console.log('Connection closed');
+  //   });
   });
 
 });
